62671d1a93036062494c0687a7d59f55bd9075ac
luchangli <LuChangli@windows10.microdone.cn>
2020-05-22 18:22:27 +0800

    错误提示

--- MedasIoT/medas-iot-rbac/src/main/java/com/foxconn/iot/repository/ErrorCodeRepository.java
+++ MedasIoT/medas-iot-rbac/src/main/java/com/foxconn/iot/repository/ErrorCodeRepository.java
@@ -1,9 +1,18 @@
 package com.foxconn.iot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.foxconn.iot.entity.ErrorCodeEntity;
 
-public interface ErrorCodeRepository extends JpaRepository<ErrorCodeEntity, Integer> {
+
+public interface ErrorCodeRepository extends JpaRepository<ErrorCodeEntity, Long> {
 	
+	ErrorCodeEntity findById(long id);
+	
+	@Modifying
+	@Query(value = "delete from ErrorCodeEntity a where a.id=:id and a.company.id=:company")
+	void updateStatusByIdAndCompany(@Param("id") long id, @Param("company") long companyId);
 }
--- MedasIoT/medas-iot-rbac/src/main/java/com/foxconn/iot/service/impl/ErrorCodeServiceImpl.java
+++ MedasIoT/medas-iot-rbac/src/main/java/com/foxconn/iot/service/impl/ErrorCodeServiceImpl.java
@@ -1,39 +1,57 @@
 package com.foxconn.iot.service.impl;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.foxconn.iot.dto.ErrorCodeDto;
+import com.foxconn.iot.entity.ErrorCodeEntity;
+import com.foxconn.iot.exception.BizException;
 import com.foxconn.iot.repository.ErrorCodeRepository;
 import com.foxconn.iot.service.ErrorCodeService;
+import com.foxconn.iot.support.Snowflaker;
+import com.mysql.cj.util.StringUtils;
 
 @Service
 public class ErrorCodeServiceImpl implements ErrorCodeService {
 	
 	@Autowired
 	private ErrorCodeRepository errorCodeRepository;
-	
 
 	@Override
 	public void create(ErrorCodeDto error) {
-		
+		ErrorCodeEntity entity = new ErrorCodeEntity();
+		BeanUtils.copyProperties(error, entity);
+		long id = Snowflaker.getId();
+		entity.setId(id);
+		errorCodeRepository.save(entity);
 	}
 
 	@Override
 	public void save(ErrorCodeDto error) {
+		ErrorCodeEntity entity = errorCodeRepository.findById(error.getId());
+		if (entity == null) {
+			throw new BizException("Invalid error code");
+		}
 		
+		if (!StringUtils.isNullOrEmpty(error.getMsg())) {
+			entity.setMsg(error.getMsg());
+		}
+		errorCodeRepository.save(entity);
 	}
 
 	@Override
+	@Transactional
 	public void deleteById(long id) {
-		// TODO Auto-generated method stub
-		
+		errorCodeRepository.deleteById(id);
 	}
 
 	@Override
+	@Transactional
 	public void deleteByIdAndCompany(long id, long companyId) {
-		// TODO Auto-generated method stub
-		
+		errorCodeRepository.updateStatusByIdAndCompany(id, companyId);
 	}
 
 }
